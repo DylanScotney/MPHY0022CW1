d29f670ec858d57e8ba487a9595f11e43219c06f
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-02-12 14:43:41 +0000

    Adding unit test to ensure getMockData() returns the correct number of data points

--- Testing/mphyLeastSquaresSolverTests.cpp
+++ Testing/mphyLeastSquaresSolverTests.cpp
@@ -25,4 +25,20 @@ TEST_CASE("Instansiation of linDataCreator", "linDataCreator") {
 	REQUIRE(dataComm);
 }
 
+TEST_CASE("Random data gen", "linDataCreator") {
+	std::unique_ptr<linDataCreator> data(new linDataCreator);
+	std::vector<std::pair<double, double>> mockData;
+
+	int numData = 10;
+	mockData = data->getMockData(numData);
+
+	REQUIRE(static_cast<int>(mockData.size()) == numData);
+
+
+	/*for (auto i = mockData.begin(); i != mockData.end(); i++) {
+		std::cout << i->first << " " << i->second << std::endl;
+	}*/
+
+}
+
 } // end namespace mphy
