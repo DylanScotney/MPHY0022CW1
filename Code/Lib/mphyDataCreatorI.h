635cce6ea35a2bbac2c042d5289a0ac35ffa6834
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-02-12 10:50:36 +0000

    Adding virtual interface for data reciever

---
+++ Code/Lib/mphyDataCreatorI.h
@@ -0,0 +1,21 @@
+/*=============================================================================
+
+MPHYG0022CW1: CW1, 2019: Linear Regression.
+
+Copyright (c) University College London (UCL). All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.
+
+See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include <vector>
+
+class dataReceiver {
+public:
+	virtual std::vector<std::pair<double, double> > GetData() = 0;
+};
+
