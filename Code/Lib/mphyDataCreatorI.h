5141756c352d3d39b7051bb504cb933ba22e8212
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-02-14 10:28:12 +0000

    adding default constructor and constructor with parameters

--- Code/Lib/mphyDataCreatorI.h
+++ Code/Lib/mphyDataCreatorI.h
@@ -15,8 +15,28 @@ See LICENSE.txt in the top level directory for details.
 #include <vector>
 
 class dataInterface {
+private:
+
 public:
-	const virtual std::vector<std::pair<double, double> > GetData() = 0;
+	double xRange[2], yRange[2];
+	int numData;
+
+	dataInterface() {
+		xRange[0] = 0;
+		xRange[1] = 100;
+		yRange[0] = 0;
+		yRange[1] = 100;
+		numData = 10000;
+	}
+	dataInterface(double x0, double xf, double y0, double yf, int n) {
+		xRange[0] = x0;
+		xRange[1] = xf;
+		yRange[0] = y0;
+		yRange[0] = yf;
+		numData = n;
+	}
+
+	//~dataInterface();
+	virtual std::vector< std::pair<double, double> > GetData() = 0;
+
 };
-
-
