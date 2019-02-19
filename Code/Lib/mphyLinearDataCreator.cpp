11e31bcde9715dce9b0337970f1165c42deaff56
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-02-19 18:14:08 +0000

    Correcting how random data is generated

--- Code/Lib/mphyLinearDataCreator.cpp
+++ Code/Lib/mphyLinearDataCreator.cpp
@@ -21,14 +21,21 @@ namespace mphy {
 vecPairdd linDataCreator::GetData()
 {
 	/*returns a 2 x num dataset of randomly distributed (y, x) points*/
+	// y = c1*x + c2 + noise
+	double c1, c2, x;
+
+	c1 = 2; 
+	c2 = 1;
 
 	int seed = 1;
 	std::mt19937 mt(seed);
 	std::uniform_real_distribution<double> xdist(xRange[0], xRange[1]);
-	std::uniform_real_distribution<double> ydist(yRange[0], yRange[1]);
+	std::uniform_real_distribution<double> noiseDist(-noise, noise);
+
 
 	for (int i = 0; i < numData; i++) {
-		mock_data.push_back(std::make_pair(xdist(mt), ydist(mt)));
+		x = xdist(mt);
+		mock_data.push_back(std::make_pair(x, c1*x + c2 + noiseDist(mt)));
 	}
 
 	return mock_data;
