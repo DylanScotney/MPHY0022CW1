754b5e3f36fd82323ae531c65617238bf18af4aa
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-02-21 11:12:55 +0000

    Moving members from dataInterface class to linDataCreator class

--- Code/Lib/mphyDataCreatorI.h
+++ Code/Lib/mphyDataCreatorI.h
@@ -20,30 +20,9 @@ See LICENSE.txt in the top level directory for details.
 namespace mphy {
 
 class dataInterface {
-private:
-	double xRange[2], noise;
-	int numData;
-
 public:
 
-	dataInterface() {
-		xRange[0] = 0;
-		xRange[1] = 100;
-		noise = 5;
-		numData = 10000;
-	}
-	dataInterface(double x0, double xf, double Noise, int n) {
-		xRange[0] = x0;
-		xRange[1] = xf;
-		noise = Noise;
-		numData = n;
-	}
-
 	virtual vecPairdd GetData() = 0;
-	double getXMin() { return xRange[0]; }
-	double getXMax() { return xRange[1]; }
-	double getNoise() { return noise; }
-	int getNumData() { return numData; }
 };
 
 } // end namespace mphy
--- Code/Lib/mphyLinearDataCreator.h
+++ Code/Lib/mphyLinearDataCreator.h
@@ -23,18 +23,37 @@ namespace mphy {
 class linDataCreator : public dataInterface {
 private:
 	double c1, c2;  //y = c1 * x + c2 + noise
+	double xRange[2], noise;
+	int numData;
 	vecPairdd mock_data;
 public:
+
 	linDataCreator() {
+		xRange[0] = 0;
+		xRange[1] = 100;
+		noise = 5;
+		numData = 10000;
 		c1 = 2;
 		c2 = 1;
 	};
-	linDataCreator(double xCoefficient, double intercept) {
+
+	linDataCreator(double xCoefficient, double intercept, double x0, double xf, double Noise, int n) {
+		xRange[0] = x0;
+		xRange[1] = xf;
+		noise = Noise;
+		numData = n; 
 		c1 = xCoefficient;
 		c2 = intercept;
 	};
+
 	~linDataCreator() {};
+
 	vecPairdd GetData() override;
+
+	double getXMin() { return xRange[0]; }
+	double getXMax() { return xRange[1]; }
+	double getNoise() { return noise; }
+	int getNumData() { return numData; }
 };
 
 } // end namespace mphy
