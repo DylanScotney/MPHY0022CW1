422747f7de90436b02586b6afae3d17c2327af76
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-02-21 19:49:56 +0000

    Adding concrete class which loads data from file

---
+++ Code/Lib/mphyFileLoaderDataCreator.cpp
@@ -0,0 +1,19 @@
+#include "mphyFileLoaderDataCreator.h"
+/*=============================================================================
+
+MPHYG0022CW1: CW1, 2019: Linear Regression.
+
+Copyright (c) University College London (UCL). All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.
+
+See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+vecPairdd mphy::dataLoadFile::GetData()
+{
+	return vecPairdd();
+}
---
+++ Code/Lib/mphyFileLoaderDataCreator.h
@@ -0,0 +1,22 @@
+/*=============================================================================
+
+MPHYG0022CW1: CW1, 2019: Linear Regression.
+
+Copyright (c) University College London (UCL). All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.
+
+See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#ifndef mphyFileLoaderDataCreator_h
+#define mphyFileLoaderDataCreator_h
+
+namespace mphy {
+
+} // end namespace mphy
+
+#endif
